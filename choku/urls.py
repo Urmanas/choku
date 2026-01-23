8ee49e00fd4a0aeb2a3b4e30b7ef5648ed55267f
Urmanas <Urmanas.19@gmail.com>
2026-01-23 10:21:44 +0600

    Created models for orders app

--- choku/urls.py
+++ choku/urls.py
@@ -5,4 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('core.urls')),
     path("admin/", admin.site.urls),
+    path('tours/', include('tours.urls')),
 ]
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from tours.models import Tour
 
-# Create your models here.
+class Order(models.Model):
+    tour = models.ForeignKey(Tour, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    email = models.EmailField(blank=True)
+    people = models.PositiveIntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.tour.title}"
+        
